8563570bb9e0277bd986474038ad6f5be5541dff
markoptionb <optionbsupport@gmail.com>
2021-05-27 15:58:44 +1000

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr. Chocolate", 13, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 13, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 13, "F", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 13, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 13, "F", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 13, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 13, "M", "brown", "calm", true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (null, 13, "M", "brown", "calm", true);
+
+
